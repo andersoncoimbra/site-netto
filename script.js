ea775e7b4bdad69b5e634f87b987010fb2ba2c5c
leo silva <leonardolass@hotmail.com.br>
2020-09-13 22:33:10 -0300

    add cardas e imgs novas

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+var feed = $('.yottie-widget-contents')[0];
+    $('.yottie-widget-inner').html(feed);
+    
 $('.owl-carousel').owlCarousel({
     items:1,
     lazyLoad:true,
     loop:true,
     margin:10
 });
+setInterval($('.eapps-link').hide(),1000);
